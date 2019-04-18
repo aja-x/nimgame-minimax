1492ecfac2eaf8d25416600a82bc94f60c42dff2
aja-x <ajax.soft.en@gmail.com>
2019-04-18 13:19:49 +0700

    Change Tree.py

--- Tree.py
+++ Tree.py
@@ -2,38 +2,52 @@ from anytree import Node, RenderTree, findall
 
 
 class Tree:
-    def __init__(self, root_value, player):
-        self.tree = None
+    def __init__(self, root_value, player_turn):
         self.root_value = root_value
-        self.player = player  # player 0 = Human, 1 = Computer
+        self.player_turn = player_turn  # player_turn: True = Human, False = Computer
         self.current_state = 0
         self.check_state = True
-        self.render_tree()
+        if self.player_turn:
+            self.tree = [Node(self.current_state, node_value=[self.root_value], is_final=False, state_value=1)]
+        else:
+            self.tree = [Node(self.current_state, node_value=[self.root_value], is_final=False, state_value=-1)]
+        # self.render_tree()
 
     def render_tree(self):
-        if self.player == 0:
-            self.tree = [Node(self.current_state, node_value=[0, [self.root_value]])]
-        else:
-            self.tree = [Node(self.current_state, node_value=[1, [self.root_value]])]
-        self.current_state += 1
-
-        # current_node = RenderTree(self.tree).by_attr("node_value")
         while self.check_state:
-            for i in findall(self.tree[0], filter_=lambda n: n.depth == 1):
+            self.player_turn = not self.player_turn
+            for i in findall(self.tree[0], filter_=lambda n: n.depth == self.current_state):
+                print(i)
                 for j in self.count_child(i):
-                    pass  # mentok, lanjut besok
+                    self.tree.append(Node(len(self.tree)+1, parent=self.tree[i.name], node_value=j, state_value=None))
+            self.current_state += 1
+            # print(RenderTree(self.tree[0]))
 
     def count_child(self, node):
         value = []
-        for i in node.node_value[1]:
-            deduction = 1
-            while i > deduction:
-                value.append(i - 1)
-                deduction += 1
-                i -= 1
+        for k in node.node_value:
+            j = 0
+            temp = k
+            while temp - 1 >= j + 1:
+                value.append(self.set_child_value(node.node_value, k, j + 1))
+                temp -= 1
+                j += 1
+        return value
+
+    def set_child_value(self, node, k, j):
+        value = []
+        for i in node:
+            if i == k:
+                value.append(k - j)
+                value.append(j)
+            else:
+                value.append(i)
         value.sort(reverse=True)
         return value
 
-    # def make_node(self, value, parent):
-    #     self.tree.append(Node(self.calculate_value(value), parent=parent))
-    #
+    def set_node(self, parent_node, ):
+        pass
+
+
+tree = Tree(9, True)
+tree.render_tree()
--- test.py
+++ test.py
@@ -1,9 +1,28 @@
 from anytree import Node, RenderTree, findall
 
 
+def set_child(node, k, j):
+    value = []
+    for i in node:
+        if i == k:
+            value.append(k-j)
+            value.append(j)
+        else:
+            value.append(i)
+    value.sort(reverse=True)
+    return value
+
+
 def count_child(node):
-    for k in node.node_value[1]:
-        print(k)
+    value = []
+    for k in node.node_value[0]:
+        j = 0
+        temp = k
+        while temp - 1 >= j + 1:
+            value.append(set_child(node.node_value[0], k, j + 1))
+            temp -= 1
+            j += 1
+    print(value)
 
 
 def count_child2(node):
@@ -19,31 +38,72 @@ def count_child2(node):
     print(value)
 
 
+def count_child3(node):
+    value = []
+    for j in node.node_value[0]:
+        # deduction = 1
+        # node.node_value[1].pop(j)
+        # print(node.node_value[1])
+        # print("hubla\n")
+        temp = node.node_value[0]
+        for k in temp:
+            deduction = 1
+            l = k
+            while (l > deduction + 1) and (l != 1):
+                value.append(node.node_value[0])
+                value[len(value) - 1].remove(l)
+                for h in value[len(value)-1]:
+                    print(str(k)+str(h))
+                    break
+                # print("+"+str(l)+"+"+str(k)+"\n")
+                # value[len(value) - 1].remove(k)
+                # value[len(value) - 1].append(l-1)
+                # value[len(value) - 1].append(deduction)
+                deduction += 1
+                l -= 1
+
+        # while j > deduction:
+        #     value.append(node.node_value[0])
+        #     value[deduction - 1].remove(j)
+        #     value[deduction - 1].append(j - 1)
+        #     value[deduction - 1].append(deduction)
+        #     value[deduction - 1].sort(reverse=True)
+        #     deduction += 1
+        #     j -= 1
+    print(value)
+
+
+def set_child_value(node_value):
+    pass
+
+
 root_value = 9
 current_state = 0
-tree = [Node(current_state, node_value=[0, [root_value]])]
+tree = [Node(current_state, node_value=[[root_value]])]
 current_state += 1
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[1, [8, 1]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[1, [7, 2]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[1, [6, 3]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[1, [5, 4]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[8, 1]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[7, 2]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[6, 3]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[5, 4]]))
 
 current_state += 1
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[0, [7, 1, 1]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[0, [6, 2, 1]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[0, [5, 3, 1]]))
-tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[0, [4, 4, 1]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[7, 1, 1]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[6, 2, 1]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[5, 3, 1]]))
+tree.append(Node(len(tree), parent=tree[current_state - 1], node_value=[[4, 4, 1]]))
 
 # print(RenderTree(tree[0]).by_attr())
 # print(RenderTree(tree[0]).by_attr(lambda n: n.name if n.depth == 1 else None))
 # print(RenderTree(tree[0]).by_attr(lambda n: n.depth == 1))
 
-tree.append(Node(len(tree), parent=tree[current_state], node_value=[2, [6, 3]]))
+tree.append(Node(len(tree), parent=tree[current_state], node_value=[[6, 3]]))
 
-for i in findall(tree[0], filter_=lambda n: n.depth == 2):
-    print(i.name)
-    print(i.node_value[1])
-    count_child2(i)
+for i in findall(tree[0], filter_=lambda n: n.depth == 1):
+    # print(i)
+    # print(i.name)
+    print(i.node_value[0])
+    count_child(i)
+    print("\n")
     # for j in i.node_value[1]:
     #     print(j)
     # print("\n")
